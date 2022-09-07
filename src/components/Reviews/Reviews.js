8c19a4acf7b462a29a709fee6351cf39b433d871
Valeriy Semko <valsemko74@gmail.com>
2022-09-07 19:45:55 +0300

    make Reviews

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { fetchReviews } from '../../servises/moviesApi';
 
 export const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const [error, setError] = useState('');
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    async function getReviews() {
+      try {
+        const data = await fetchReviews(movieId);
+        setReviews(data.results);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    getReviews();
+  }, [movieId]);
+
   return (
-    <div>Reviews</div>
+    <>
+      {error && <Navigate to="/movies" replace />}
+      {reviews.length === 0
+        ? (<p>We don`t have any reviews on this movie</p>)
+        : (<ul>
+            {reviews.map(review => (
+              <li key={review.id}>
+                <h3>Author: {review.author}</h3>
+                <p>{review.content}</p>
+              </li>
+            ))}
+          </ul>)
+      }
+    </>
   )
 }
